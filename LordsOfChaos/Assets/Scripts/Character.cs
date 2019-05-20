05bb63c0c2aed8e1c43df879f1c90207cfee6eb7
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2019-05-20 13:11:10 +0100

    Project Setup
    Player movement added

---
+++ LordsOfChaos/Assets/Scripts/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour                 // Class is abstract as it cannot exist alone, 
+                                                                // it must have classes that derive from it.
+{
+
+    protected int health;
+    protected int movement;
+    protected int constitution;
+    protected int attack;
+    protected int defense;
+
+    protected Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        {
+            transform.Translate(direction);
+        }
+    }
+
+
+}
---
+++ LordsOfChaos/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Player : Character
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GetInput();
+
+        base.Update();
+    }
+
+    // Listen to player's input
+    private void GetInput()
+    {
+        direction = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction += (Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction += (Vector3.back);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction += (Vector3.left);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction += (Vector3.right);
+        }
+    }
+}
