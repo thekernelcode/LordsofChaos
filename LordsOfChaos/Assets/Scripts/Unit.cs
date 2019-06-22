434ad60c13c1c525973eb7c001d16e42ac539eac
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2019-06-22 12:38:20 +0100

    Update 220619

---
+++ LordsOfChaos/Assets/Scripts/Unit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    public string name;
+    public float movement;
+    public float strength;
+    public float health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
