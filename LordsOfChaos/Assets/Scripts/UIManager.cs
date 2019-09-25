16848ab40f6f0745503024483dbb79f7af868d41
TheKernelCode <thekernelcode@gmail.com>
2019-09-25 11:15:34 +0100

    Minor tweaks

--- LordsOfChaos/Assets/Scripts/UIManager.cs
+++ LordsOfChaos/Assets/Scripts/UIManager.cs
@@ -29,13 +29,13 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ShowSpellFrame(GameObject unitToCast)
